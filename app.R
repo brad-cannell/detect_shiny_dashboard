45ef16430f240d640aa497255e176aa154741ce4
Brad Cannell <brad.cannell@gmail.com>
2020-10-29 13:39:46 -0500

    Working on #2, First working draft of dashboard
    2020-10-29: I finally have a working app. Just need to make sure it works behind the UTH firewall and then improve.

--- app.R
+++ app.R
@@ -1,89 +1,104 @@
 # =============================================================================
 # DETECT Shiny Dashboard
-# Created: 2020-08-24
+# Created: 2020-10-28
+# 
+# Note: that there is a difference between using tableOutput and 
+# dataTableOutput. 
+# One important difference is that tableOutput doesn't format dates as date 
+# values by default. 
+# https://stackoverflow.com/questions/21548843/r-shiny-different-output-between-rendertable-and-renderdatatable
+
+# Note: must use DT::dataTableOutput and DT::renderDataTable instead of
+# shiny::dataTableOutput and shiny::renderDataTable if your data includes date
+# columns.
+# https://community.rstudio.com/t/data-table-issue-while-rendering-the-shiny-page-datatables-warning-table-id-datatables-table-0-requested-unknown-parameter/44016/4
 # =============================================================================
 
-# Packages
+# Load packages
+# -------------
 library(shiny)
+library(shinydashboard)
+library(readr)
 library(dplyr)
-library(tidyr)
-library(ggplot2)
 library(DT)
+library(ggplot2)
+library(plotly)
 
-# Datasets
-gift_card <- read.csv("data/Gift Card.csv")
-call_log <- read.csv("data/Participant Call Log.csv")
 
-call_log <- call_log %>% 
-    mutate(
-        # Change classes
-        call_date            = as.Date(CallDate, "%m/%d/%Y")
-    )
+# Load data
+# ---------
+# For now, just import the data that I cleaned in analysis_01_recruitment_metrics.Rmd.
+# Eventually, I will need to figure out where to store all that data cleaning 
+# code and how best to run it when I refresh the data.
+calls_per_day_w_scheduled <- read_rds("/Users/bradcannell/Desktop/calls_per_day_w_scheduled.rds")
 
-# Define User Interface
-ui <- fluidPage(
+
+
+# =============================================================================
+# Create the UI
+# =============================================================================
+ui <- dashboardPage(
+    dashboardHeader(title = "DETECT Dashboard"),
     
-    titlePanel("DETECT Recruitment Dashboard"),
-    
-    sidebarLayout(
-        
-        sidebarPanel(
-            
-            selectInput("main_select", 
-                        label = "What would you like to see?",
-                        choices = c("Call Log", "Gift Card")),
-            
-            br(),
-            
-            dateRangeInput('date_range',
-                           label = 'Select date range (inclusive): yyyy-mm-dd',
-                           start = Sys.Date() - 30 , end = Sys.Date()
-            ),
-            
-            br(),
-            
-            img(src = "dr_cannell_headshot.jpg", height = 250, width = 250), 
-            
-            h5("Principal Investigator", align = "center"),
-            h5("Dr. M. Brad Cannell", align = "center")
-            
-            
-            
-        ),
-        
-        mainPanel(
-            h3(textOutput("main_object")),
-            br(),
-            h3(textOutput("selected_dates")),
-            br(),
-            DT::dataTableOutput(outputId = "table"), style = "height:500px; overflow-y: scroll;overflow-x: scroll;"
-            
-            
+    # Inputs
+    # Create an input area in the user interface to select the dates to filter on
+    # Using fluidRow stacks the date selector above the data frames instead of to
+    # the left of the data frames.
+    dashboardSidebar(
+        dateRangeInput(
+            "dates",
+            "What dates do you want (inclusive)?",
+            # Set default start and end dates
+            start = "2019-08-01",
+            end = Sys.Date()
         )
-        
+    ),
+    dashboardBody(
+        # Make a chart from the filtered data
+        fluidRow(
+            plotlyOutput("plot")
+        )
     )
-    
 )
 
-# Define server logic
-server <- function(input, output) {
+# =============================================================================
+# Create the server function
+# =============================================================================
+server <- function(input, output, session) {
     
-    output$main_object <- renderText({
-        paste("You are viewing ", input$main_select)
+    # Create a reactive version of the filtered data frame
+    filtered_df <- reactive({
+        calls_per_day_w_scheduled %>% 
+            filter(Date >= input$dates[1] & Date <= input$dates[2])
     })
     
-    output$selected_dates <- renderText({
-        paste("You have selected the dates: ", as.character(input$date_range), collapse = " to ")
-    })
-    
-    output$table <- DT::renderDataTable({
-        if(input$main_select == "") { return() }
-        
-        call_log %>% 
-            filter(call_date >= input$date_range[1],  call_date <= input$date_range[2])
+    # The left-hand side of the assignment operator (<-), output$ID, indicates 
+    # that you’re providing the recipe for the Shiny output with the matching ID.
+    # The right-hand side of the assignment uses a specific render function to 
+    # wrap some code that you provide.
+    # Add the calls/scheduled per day plot to the UI
+    output$plot <- renderPlotly({
+        filtered_df() %>%
+            ggplot(aes(Date, n_calls)) +
+                geom_line(color = "#8a8a8a") +
+                geom_point(aes(color = n_scheduled_f)) +
+                scale_x_date("Date", date_label = "%b-%y") +
+                scale_y_continuous("Number of Calls") +
+                scale_color_manual(
+                    "F/U Scheduled",
+                    values = c(
+                        "#8a8a8a", "#F2E750", "#F2B807", "#F28705", "#C52104", 
+                        "#a60303"
+                    ),
+                    drop = FALSE
+                ) +
+                theme_classic() +
+                theme(legend.title = element_text(size = 8))
     })
     
 }
 
-# Run the app
-shinyApp(ui = ui, server = server)
+# =============================================================================
+# Render the app
+# =============================================================================
+shinyApp(ui, server)
